13bc6fe2fcfbc01176c762bdc60971e7033cac91
Ibvu <159502883+ibvusito@users.noreply.github.com>
2024-02-23 14:43:46 -0500

    Update descargas-tiktokstalk.js

--- plugins/descargas-tiktokstalk.js
+++ plugins/descargas-tiktokstalk.js
@@ -1,51 +1,28 @@
-import fetch from 'node-fetch'
-let handler = async(m, { conn, text, command, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}${mid.smsTikTok6}\n*${usedPrefix + command} Gata_Dios*`, m)
-try {
-let res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=${lolkeysapi}`)
-let res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`
-let json = await res.json()
-if (res.status !== 200) throw await res.text()
-if (!json.status) throw json
-let thumb = await (await fetch(json.result.user_picture)).buffer()
-let gata = `👤 ${mid.user} 
-${json.result.username}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✨ ${mid.name}
-${json.result.nickname}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-✅ ${mid.smsinsta1}
-${json.result.followers}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-❇️ ${mid.smsinsta2}
-${json.result.followings}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-❤️ ${mid.smsinsta5}
-${json.result.likes}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-🎁 ${mid.smsinsta3}
-${json.result.video}
-┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-👀 ${mid.smsinsta4}
-${json.result.bio}
-`.trim()
-await conn.sendFile(m.chat, res2, 'error.jpg', gata, m, false)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}}
-handler.help = ['tiktokstalk'].map(v => v + ' <username>')
-handler.tags = ['stalk']
-handler.command = /^(tiktokstalk|ttstalk)$/i
-handler.exp = 48
-export default handler
-
-/*let info = `💖 *Infórmate sobre las Novedades y recuerda tener la última versión.*\n\n💝 *Find out about what's new and remember to have the latest version.*
-  `.trim()
-  
-await conn.sendHydrated(m.chat, info, wm, null, ig, '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢', null, null, [
-['𝙈𝙚𝙣𝙪 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨 🌀', '#descargasmenu'],
-['𝙈𝙚𝙣𝙪 𝘾𝙤𝙢𝙥𝙡𝙚𝙩𝙤 | 𝙁𝙪𝙡𝙡 𝙈𝙚𝙣𝙪 ✨', '.allmenu'],
-['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
-], m,)  */
+import fetch from 'node-fetch';
+const handler = async (m, {conn, text}) => {
+  if (!text) return conn.reply(m.chat, '*[❗𝐈𝐍𝐅𝐎❗] 𝙸𝙽𝚂𝙴𝚁𝚃𝙴 𝙴𝙻 𝙲𝙾𝙼𝙰𝙽𝙳𝙾 𝙼𝙰𝚂 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚄𝙽 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙳𝙴 𝚃𝙸𝙺𝚃𝙾𝙺*', m);
+  try {
+    const res = await fetch(`https://api.lolhuman.xyz/api/stalktiktok/${text}?apikey=${lolkeysapi}`);
+    const res2 = `https://api.lolhuman.xyz/api/pptiktok/${text}?apikey=${lolkeysapi}`;
+    const json = await res.json();
+    if (res.status !== 200) throw await res.text();
+    if (!json.status) throw json;
+    const thumb = await (await fetch(json.result.user_picture)).buffer();
+    const Mystic = `
+*𝚄𝚂𝚄𝙰𝚁𝙸𝙾:* ${json.result.username}
+*𝙽𝙾𝙼𝙱𝚁𝙴:* ${json.result.nickname}
+*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚁𝙴𝚂:* ${json.result.followers}
+*𝚂𝙴𝙶𝚄𝙸𝙳𝙾𝚂:* ${json.result.followings}
+*𝙻𝙸𝙺𝙴𝚂:* ${json.result.likes}
+*𝚅𝙸𝙳𝙴𝙾𝚂:* ${json.result.video}
+*𝙳𝙴𝚂𝙲𝚁𝙸𝙿𝙲𝙸𝙾𝙽:* ${json.result.bio}
+`.trim();
+    conn.sendFile(m.chat, res2, 'error.jpg', Mystic, m, false);
+  } catch (e) {
+    throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙽𝙾 𝚂𝙴 𝙴𝙲𝙾𝙽𝚃𝚁𝙾 𝙴𝙻 𝙽𝙾𝙼𝙱𝚁𝙴 𝙳𝙴 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 𝙸𝙽𝙶𝚁𝙴𝚂𝙰𝙳𝙾*';
+  }
+};
+handler.help = ['tiktokstalk'].map((v) => v + ' <username>');
+handler.tags = ['stalk'];
+handler.command = /^(tiktokstalk|ttstalk)$/i;
+export default handler;
