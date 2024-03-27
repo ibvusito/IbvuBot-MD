b85678f72b9e035d408c57808bdfc7ceebd6c5e6
Xyzco <159502883+ibvusito@users.noreply.github.com>
2024-03-27 16:17:20 -0500

    Create so-lista.js

---
+++ plugins/so-lista.js
@@ -0,0 +1,130 @@
+//CREDITOS PARA >> https://github.com/BrunoSobrino
+
+let handler = m => m
+handler.all = async function (m) {
+let chat = global.db.data.chats[m.chat]
+//let pp = gataVidMenu.getRandom()
+//let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+global.db.data.users[m.sender].money += 50
+global.db.data.users[m.sender].exp += 50  
+    
+if (/^El mamut|Un mamut|🫂$/i.test(m.text) && chat.audios) {  
+if (!db.data.chats[m.chat].audios && m.isGroup) throw 0    
+let vn = './media/elmamut.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+  
+if (chat.audios && m.text.match(/(baneado|Baneado|baneada|🤫)/gi)) {    
+let vn = './media/baneado.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+    
+if (chat.audios && m.text.match(/(alan que hiciste|pendejada)/gi)) {    
+let vn = './media/alanquependejadahiciste.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+    
+if (chat.audios && m.text.match(/(sexo|algo anda mal|mal)/gi)) {    
+let vn = './media/algoandamal.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+    
+if (chat.audios && m.text.match(/(Carlitos|porno)/gi)) {    
+let vn = './media/carlitos.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+    
+if (chat.audios && m.text.match(/(Pasa pack|Foto teta)/gi)) {    
+let vn = './media/Elmo.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+    
+if (chat.audios && m.text.match(/(Contexto|CONTEXTO|contexto|Pasen contexto|PASEN CONTEXTO|pasen contexto|Y el contexto|Y EL CONTEXTO|y el contexto)/gi)) {    
+let vn = './media/contexto.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+
+if (chat.audios && m.text.match(/(Corre perra|corre)/gi)) {    
+let vn = './media/correperracorre.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+    
+if (chat.audios && m.text.match(/(siu|calamardo|el bicho|siuuu)/gi)) {    
+let vn = './media/eslomasestupidoqueheoido.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+
+if (chat.audios && m.text.match(/(Enloqueciste|joder|esponja)/gi)) {    
+let vn = './media/esponjaenloqueciste.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔 🔞`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen9, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+
+if (chat.audios && m.text.match(/(:c)/gi)) {    
+let vn = './media/toma.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}  
+
+if (chat.audios && m.text.match(/(No era parte del trato|Esto no era)/gi)) {    
+let vn = './media/estonoerapartedeltrato.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}   
+
+if (chat.audios && m.text.match(/(Pipipi|Help|Oe)/gi)) {    
+let vn = './media/pipipi.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}   
+
+if (chat.audios && m.text.match(/(Ya es toda)/gi)) {    
+let vn = './media/yaestodawey.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}   
+
+if (chat.audios && m.text.match(/(Estoy feliz|Ralph|😡)/gi)) {    
+let vn = './media/Estoyfeliz.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}   
+    
+if (chat.audios && m.text.match(/(Cancer|No podia fumar|queria fumar)/gi)) {    
+let vn = './media/Queriafumar.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}   
+
+if (chat.audios && m.text.match(/(Queria volar|volar)/gi)) {   
+let vn = './media/Volar.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen2, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}   
+
+if (chat.audios && m.text.match(/(Modo diablo|😈|🤗|👋)/gi)) {
+//let vn = './media/Mododiablo.mp3'
+let vn = './media/Mododiablo.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": wm, "body": `𝑿𝒚𝒛𝒄𝒐 𝒒𝒏 𝒎𝒂𝒔`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen6, "sourceUrl": `https://github.com/ibvusito/XyzcoBot-MD`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+    
+if (chat.audios && m.text.match(/(laoracion|La biblia|La oración|La biblia|La oración|la biblia|La Biblia|oremos|recemos|rezemos|🙌|👏|🙏)/gi)) {   let vn = './media/La biblia.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+"thumbnailUrl": null,"thumbnail": imagen6, "sourceUrl": `https://github.com/ibvusito/XyzcoBot-MD`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+    
+if (chat.audios && m.text.match(/(NO DIGAS ESO PAPU|no digas eso|No gigas eso papu|NO PAPU|No papu|NO papu|no papu)/gi)) {    
+let vn = './media/nopapu.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+"thumbnailUrl": null,"thumbnail": imagen6, "sourceUrl": `https://github.com/ibvusito/XyzcoBot-MD`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+    
+if (chat.audios && m.text.match(/(te amo|teamo)/gi)) {    
+let vn = './media/te-amo.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+"thumbnailUrl": null,"thumbnail": imagen6, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+
+if (chat.audios && m.text.match(/(Casi me cago)/gi)) {    
+let vn = './media/casimecagodelsusto.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+"thumbnailUrl": null,"thumbnail": imagen6, "sourceUrl": `https://www.instagram.com/xyzco0/`, "showAdAttribution": true}}, ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
+
+if (chat.audios && m.text.match(/(Gay|🏳️‍🌈)/gi)) {
+let vn = './media/DiagnosticadoConGay.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+   
+return !0 }
+export default handler
+function pickRandom(list) {
+return list[Math.floor(Math.random() * list.length)]}
